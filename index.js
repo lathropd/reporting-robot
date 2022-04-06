48a43add8a5f22ce7c1280cf584d2d7323b3b9b1
Daniel Lathrop <daniel.lathrop@gmail.com>
2022-04-06 11:41:26 -0500

    pseudocode

---
+++ index.js
@@ -0,0 +1,18 @@
+let requests = require('requests');
+let cheerio = require('cheerio');
+
+let url = 'https://apps.polkcountyiowa.gov/PolkCountyInmates/CurrentInmates/Last24HrsJsonData';
+
+// psuedocode
+// load existing data
+// check last item
+// get json
+// id new items
+// scrape each
+// check new list for murders
+// write complete list
+// email if there are new murders
+
+
+
+
